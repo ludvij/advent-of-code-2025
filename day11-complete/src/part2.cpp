08901cda4e0d6aa9da3c517a920b71a003866ee8
ludvij <vijande48@gmail.com>
2025-12-12 08:34:00 +0100

    day 11

---
+++ day11-complete/src/part2.cpp
@@ -0,0 +1,88 @@
+// https://adventofcode.com/2025/day/11
+// Advent of code : 2025
+// day            : 11
+// part           : 2
+
+#include <numeric>
+#include <print>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#include <ludutils/lud_assert.hpp>
+#include <ludutils/lud_misc.hpp>
+#include <ludutils/lud_parse.hpp>
+
+#define INPUT_PATH "inputs/day11/"
+
+namespace v = std::views;
+namespace r = std::ranges;
+
+using Node = std::unordered_set<std::string_view>;
+using Graph = std::unordered_map<std::string_view, Node>;
+
+u64 cached_dfs(Graph& graph, const std::string_view origin, const std::string_view target)
+{
+    std::unordered_map<std::string_view, u64> memo;
+    const auto dfs = [&](this auto& self, const std::string_view next) -> u64 {
+        if (next == target)
+        {
+            return 1;
+        }
+        else if (auto it = memo.find(next); it != memo.end())
+        {
+            return it->second;
+        }
+
+        // clangd complains idk why
+        // const s64 res = r::fold_left(graph[next] | v::transform(self), 0UL, std::plus<>());
+        u64 res = std::transform_reduce(
+            graph[next].begin(),
+            graph[next].end(),
+            0UL,
+            std::plus<>(),
+            self
+        );
+
+        memo.emplace(next, res);
+
+        return res;
+    };
+
+    return dfs(origin);
+}
+
+u64 do_program(const char* path)
+{
+
+    const auto file = Lud::Slurp(path);
+    const auto lines = Lud::Split(file, '\n');
+
+    Graph graph;
+
+    for (const auto& line : lines)
+    {
+        const auto parts = Lud::Split(line, ':');
+        auto& node = graph[parts[0]];
+        for (const auto& children : Lud::Split(parts[1], ' '))
+        {
+            node.emplace(children);
+        }
+    }
+    return cached_dfs(graph, "svr", "fft") *
+           cached_dfs(graph, "fft", "dac") *
+           cached_dfs(graph, "dac", "out");
+    //     cached_dfs(graph, "svr", "dac") *
+    //     cached_dfs(graph, "dac", "fft") *
+    //     cached_dfs(graph, "fft", "out");
+}
+
+int main(int argc, char** argv)
+{
+    Lud::assert::eq(do_program(INPUT_PATH "test_part2.txt"), 2U);
+
+    u64 result = do_program(INPUT_PATH "input.txt");
+
+    std::println("[RESULT]: {}", result);
+    return 0;
+}
--- readme.md
+++ readme.md
@@ -1,1 +1,6 @@
-My solutions to the [Advent of code](https://adventofcode.com/) 2025 in C++
+My solutions for the [Advent of code](https://adventofcode.com/) 2025 in C++ 
+
+---
+
+Day 10 part 2 was done in python, there won't be a C++ solution until I learn how linear
+algebra works
